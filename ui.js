e11dc925d7dd5e7644ff8a81d1455c588bcb0bfc
Kemal Yalcinkaya <kemalcany@gmail.com>
2018-07-31 09:34:21 +0100

    exp: ui fix

--- ui.js
+++ ui.js
@@ -3,12 +3,17 @@ module.exports = (api, projectOptions) => {
   console.log(projectOptions)
   api.describeConfig({
     // Unique ID for the config
-    id: 'com.tailwindpurge.tipato',
+    id: 'com.tailwind-purge.ti-pa-to',
     // Displayed name
     name: 'Tailwind Purge CSS',
     // Shown below the name
     description: 'Tailwind CSS with Purge',
     // "More info" link
-    link: 'https://github.com'
+    link: 'https://github.com/ti-pa-to/vue-cli-plugin-tailwind-purge-css',
+    onRead: ({ data, cwd }) => ({
+      prompts: [
+        // Prompt objects
+      ]
+    })
   })
 }
