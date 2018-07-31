986fbcb8621a6d3118f90cd7f39ab91c776984e2
Kemal Yalcinkaya <kemalcany@gmail.com>
2018-07-31 10:23:01 +0100

    exp: pre extendPackages

--- generator.js
+++ generator.js
@@ -1,3 +1,11 @@
 module.exports = (api, options, rootOptions) => {
+  // Install Tailwind and Purge CSS packages
+  api.extendPackage({
+    devDependencies: {
+      tailwindcss: '*',
+      '@fullhuman/postcss-purgecss': '*'
+    }
+  })
+  // Render template
   api.render('./template')
 }
