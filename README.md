6e63119a897e54595cb7d1849074c65cfc0f87cb
Kemal Yalcinkaya <kemalcany@gmail.com>
2018-07-31 10:54:28 +0100

    exp: before copying assets from node_modules

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Vue CLI 3.0 plugin with Tailwind and Purge CSS
+
+### Thanks
+
+- Plugin code insipired from [vue-cli-plugin-buefy](https://github.com/buefy/vue-cli-plugin-buefy/blob/master/generator/index.js)
--- generator.js
+++ generator.js
@@ -6,6 +6,37 @@ module.exports = (api, options, rootOptions) => {
       '@fullhuman/postcss-purgecss': '*'
     }
   })
+
   // Render template
+  console.log('Cok yasa')
+  console.log(api.resolve(''))
+  console.log(
+    `${api.resolve('')}/node_modules/tailwindcss/defaultConfig.stub.js`
+  )
+  console.log('--------')
+  api.render(
+    `${api.resolve('')}/node_modules/tailwindcss/defaultConfig.stub.js`
+  )
   api.render('./template')
+
+  // Modify main
+  let importLines = `\nimport './tailwind.css'`
+  api.onCreateComplete(() => {
+    // inject to main.js
+    const fs = require('fs')
+    const ext = api.hasPlugin('typescript') ? 'ts' : 'js'
+    const mainPath = api.resolve(`./src/main.${ext}`)
+
+    // get content
+    let contentMain = fs.readFileSync(mainPath, { encoding: 'utf-8' })
+    const lines = contentMain.split(/\r?\n/g).reverse()
+
+    // inject import
+    const lastImportIndex = lines.findIndex(line => line.match(/^import/))
+    lines[lastImportIndex] += importLines
+
+    // modify app
+    contentMain = lines.reverse().join('\n')
+    fs.writeFileSync(mainPath, contentMain, { encoding: 'utf-8' })
+  })
 }
