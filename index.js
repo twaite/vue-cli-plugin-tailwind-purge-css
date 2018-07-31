4d922060681c5b91a8c4fc1477b9dc99f5e19b75
Kemal Yalcinkaya <kemalcany@gmail.com>
2018-07-31 14:06:42 +0100

    feat: working but needs improvements

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-module.exports = (api, options) => {
-  api.configureWebpack(webpackConfig => {
-    console.log('I am here thanks')
-    console.log(webpackConfig)
-    console.log('----------')
-  })
-}
+module.exports = (api, options) => {}
